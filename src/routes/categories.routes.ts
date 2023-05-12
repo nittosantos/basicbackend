a3494c42e7b3205e5b1fa0284ef4e39f761ddb11
Nitto <nitto_kun@hotmail.com>
2023-05-12 13:27:43 -0300

    feat: :sparkles: adicionando a lista de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,4 +12,10 @@ categoriesRoutes.post('/', (req, res) => {
   return res.status(201).send();
 });
 
+categoriesRoutes.get('/', (req, res) => {
+  const allCategories = categoriesRepository.list();
+
+  return res.json(allCategories);
+});
+
 export { categoriesRoutes };
