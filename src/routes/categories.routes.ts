72445351663a736baae0394b8c281d49a410d11b
Nitto <nitto_kun@hotmail.com>
2023-05-12 13:23:25 -0300

    feat: :sparkles: finalizando as criações das Categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  return res.status(201).json({ category });
+  categoriesRepository.create({ name, description });
+
+  return res.status(201).send();
 });
 
 export { categoriesRoutes };
