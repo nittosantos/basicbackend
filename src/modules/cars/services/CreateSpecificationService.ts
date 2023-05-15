6a36adb9c2baa90f60ed7920fa95c38170b5277a
Nitto <nitto_kun@hotmail.com>
2023-05-15 17:23:16 -0300

    feat: :sparkles: add a new a entiti specification

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,22 @@
-class CreateCategoryService {
-  execute() {
-    console.log('TODO');
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists');
+    }
+    this.specificationsRepository.create({ name, description });
   }
 }
 
-export { CreateCategoryService };
+export { CreateSpecificationService };
