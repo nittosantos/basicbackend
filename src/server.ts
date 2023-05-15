4b188f5fb0816c107c84b13f471186c26de99e55
Nitto <nitto_kun@hotmail.com>
2023-05-15 18:18:52 -0300

    feat: :sparkles: adda useCase to createCategories

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use('/categories', categoriesRoutes);
 app.use('/specifications', specificationsRoutes);
 
-app.listen(3333, () => console.log('Minha Vida teste'));
+app.listen(3333, () => console.log('Minha Vida Oh Amo VC !!!'));
